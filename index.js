6b73a3590a313a98a6185de0af2428ccb38c1dd5
Sajjad-Hosan <s4jj4dH034n@gmail.com>
2024-05-14 01:23:17 +0600

    pagination func

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const cors = require("cors");
-
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.t3cl13a.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
-
+app.use(cookieParser());
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -34,6 +40,20 @@ const run = async () => {
       res.send(result);
     });
     
+app.post("/volunteers", async (req, res) => {
+  const page = parseInt(req.query.page);
+  const size = parseInt(req.query.size);
+  const result = await platformServices
+    .find()
+    .skip(page * size)
+    .limit(size)
+    .toArray();
+  res.send(result);
+});
+app.get("/volunteer_posts_count", async (req, res) => {
+  const count = await platformServices.estimatedDocumentCount();
+  res.send({ count: count });
+});
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
