a27c33767b998ac2b50c70e15408e7e6845de1ad
Sajjad-Hosan <s4jj4dH034n@gmail.com>
2024-05-14 02:40:10 +0600

    sending data using gmail match

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ const client = new MongoClient(uri, {
   },
 });
 
-
 const run = async () => {
   try {
     const platformServices = client
@@ -39,27 +38,39 @@ const run = async () => {
       const result = await platformServices.find().toArray();
       res.send(result);
     });
-    
-app.post("/volunteers", async (req, res) => {
-  const page = parseInt(req.query.page);
-  const size = parseInt(req.query.size);
-  const result = await platformServices
-    .find()
-    .skip(page * size)
-    .limit(size)
-    .toArray();
-  res.send(result);
-});
-app.get("/volunteer_posts_count", async (req, res) => {
-  const count = await platformServices.estimatedDocumentCount();
-  res.send({ count: count });
-});
- // user data get,put,post,delete methods_____
-app.post("user_volunteer_post", async (req, res) => {
-  const post = req.body;
-  const result = await platformUsers.insertOne(post);
-  res.send(result);
-});
+
+    app.post("/volunteers", async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const result = await platformServices
+        .find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      res.send(result);
+    });
+    app.get("/volunteer_posts_count", async (req, res) => {
+      const count = await platformServices.estimatedDocumentCount();
+      res.send({ count: count });
+    });
+    // user data get,put,post,delete methods_____
+    app.get("/user_volunteer_posts", async (req, res) => {
+      // console.log('email:',req.query)
+      if (req.query?.email !== req.user?.email) {
+        return res.status(401).send({ message: "forbidden access" });
+      }
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await platformUsers.find(query).toArray();
+      res.send(result);
+    });
+    app.post("/user_volunteer_post", async (req, res) => {
+      const post = req.body;
+      const result = await platformUsers.insertOne(post);
+      res.send(result);
+    });
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
